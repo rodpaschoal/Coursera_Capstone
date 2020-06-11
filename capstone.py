423f47ddf594baf61d776beeea6522fba6f6961d
rodpaschoal <rodpaschoal@gmail.com>
2020-06-11 11:09:32 -0300

    tasks

--- capstone.py
+++ capstone.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 import streamlit as st
 from PIL import Image
 import os
@@ -5,12 +7,13 @@ curpath = os.path.dirname(os.path.abspath(__file__))
 #Multi-command to run streamlit in the current file: ctrl+m ctrl+s
 
 #st.write( curpath )
-st.title('Data Science @ IBM')
-st.subheader('Capstone project for professional certificate\n')
-st.write('/rodpaschoal')
+st.title('Capstone Project')
+st.subheader('IBM Data Science Professional Certificate')
+st.write('@rodpaschoal')
 st.markdown('---')
 img = Image.open(curpath + '/pegasus.jpg')
-st.image(img, caption='Let the magic happen', use_column_width=True)
+st.image(img, caption='Hello Capstone Project Course!', use_column_width=True)
+st.markdown('---')
 st.markdown('## What is this website?')
-st.markdown('I am very happy to learn how to code in  \n pyhton and use this powerful tool to  \ncreate value by means of Data Science')
+st.markdown('I am very happy to code in python  \n and use this powerful tool to  \ncreate value with Data Science')
 
