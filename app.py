f3c3506843a07fb698f0ca242e81f7649017494b
rodpaschoal <rodpaschoal@gmail.com>
2020-06-11 19:03:08 -0300

    'print hello world'

--- app.py
+++ app.py
@@ -16,4 +16,7 @@ st.image(image='pegasus.jpg', caption='Hello Capstone Project Course!', use_colu
 st.markdown('---')
 st.markdown('## What is this website?')
 st.markdown('I am very happy to code in python  \n and use this powerful tool to  \ncreate value with Data Science')
-
+st.header('Streamlit is more beautiful than Jupyter notebook')
+st.code('[1] print(\'Hello Capstone Project Course!\')')
+st.write('Hello *Capstone* Project Course! :sunglasses:')
+st.write()
